5b856cd6dcd36fc3dfcfaa3b38f6dd7ac3abff5f
nimamakhmali <nmkhmly790@gmail.com>
2025-02-14 22:44:34 +0330

    edit

--- fastapi_env/Scripts/main.py
+++ fastapi_env/Scripts/main.py
@@ -2,10 +2,20 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/index")
+@app.get("/")
 def read_root():
     return {"message": "Hello, World!"}
 
-@app.post('/index')
-def hello():
-    return 'Hello Post' 
+#@app.post('/index')
+#def hello():
+#    return 'Hello Post' 
+
+#past prameter
+@app.get('/blog/all')
+def get_blogs():
+    return {"message": f'all Blogs'}
+
+@app.get('/blog/{id}')
+def get_blog(id:int):
+    return {"message": f'Blogs {id}'}
+
